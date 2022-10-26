5dc785309dde66007f3b277aa284b6d03865c9cb
mmccullom <mwmjr150@gmail.com>
2022-10-26 13:31:04 +0100

    First Commit

---
+++ src/Die.java
@@ -0,0 +1,17 @@
+public class Die {
+	
+	private int val;
+	
+	public Die() {
+		val = (int) Math.ceil(Math.random()*6);
+	}
+	
+	public int roll() {
+		val = (int) Math.ceil(Math.random()*6);
+		return(val);
+	}
+	
+	public int getVal() {
+		return(val);
+	}
+}
